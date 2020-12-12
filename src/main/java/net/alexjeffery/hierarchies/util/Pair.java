d14fb707b81dbdee4664a3a07558afec53019a27
Alex Jeffery <alex@compilerworks.com>
2020-12-12 10:02:26 +0000

    Pair class in util.

---
+++ src/main/java/net/alexjeffery/hierarchies/util/Pair.java
@@ -0,0 +1,16 @@
+package net.alexjeffery.hierarchies.util;
+
+public class Pair<T, U> {
+
+    public final T _1;
+    public final U _2;
+
+    private Pair(T _1, U _2) {
+        this._1 = _1;
+        this._2 = _2;
+    }
+
+    public static <T, U> Pair<T, U> of(T _1, U _2) {
+        return new Pair<T, U>(_1, _2);
+    }
+}
