4fcea1e8745ee550e65b06b1d57605310911884e
Alex Jeffery <alex@compilerworks.com>
2020-12-12 10:02:56 +0000

    Make main class do a parse on a hard coded file, and print the codegen of what's parsed.

--- src/main/java/net/alexjeffery/hierarchies/main/Main.java
+++ src/main/java/net/alexjeffery/hierarchies/main/Main.java
@@ -1,8 +1,23 @@
 package net.alexjeffery.hierarchies.main;
 
+import net.alexjeffery.hierarchies.output.SystemOutCodeGenerator;
+import net.alexjeffery.hierarchies.parser.HierarchiesLexer;
+import net.alexjeffery.hierarchies.parser.HierarchiesParser;
+import net.alexjeffery.hierarchies.syntax.Declaration;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String inputFileName = "examples/lambda.hcs";
+        HierarchiesLexer lexer = new HierarchiesLexer(new ANTLRFileStream(inputFileName));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        HierarchiesParser parser = new HierarchiesParser(tokens);
+        List<Declaration> declarations = parser.declarations().out;
+        SystemOutCodeGenerator.printDeclarations(declarations);
     }
 }
